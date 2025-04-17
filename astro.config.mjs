23ecdea88c3c763271ec018a17ffdb501cfc3784
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-04-17 12:04:38 +0200

    🔧 Adding tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
